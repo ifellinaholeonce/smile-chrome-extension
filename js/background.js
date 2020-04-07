d012be7362821400c107d15509933c6a9e0dc407
Tymm <ifellinaholeonce@gmail.com>
2020-04-07 18:10:58 -0400

    Update paths

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.action === "updateIcon") {
     if (msg.value) {
-      chrome.browserAction.setIcon({ path: "smilefullico.png", tabId: sender.tab.id });
+      chrome.browserAction.setIcon({ path: "assets/smilefullico.png", tabId: sender.tab.id });
     } else {
-      chrome.browserAction.setIcon({ path: "nosmileico.png", tabId: sender.tab.id });
+      chrome.browserAction.setIcon({ path: "assets/nosmileico.png", tabId: sender.tab.id });
     }
   }
 });
